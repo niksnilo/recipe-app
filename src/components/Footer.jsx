62cd1e58068d244ccf38b6a7e2eb47d123315fdd
niksnilo <businessniks26@gmail.com>
2021-02-11 17:23:16 +0800

    Smooth Scrolling Adjustment

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import SmoothScroll from "smooth-scroll";
 
 function Footer() {
   const scroll = new SmoothScroll('a[href*="#"]', {
-    speed: 1500,
+    speed: 1100,
     speedAsDuration: true,
   });
   return (
